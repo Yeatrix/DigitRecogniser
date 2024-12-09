493112fca768c8d7b78bc2ad8e0c175a2bb6cde2
Yeatrix <162366000+Yeatrix@users.noreply.github.com>
2024-12-09 17:22:54 +0530

    Update digit_recogniser.py

--- digit_recogniser.py
+++ digit_recogniser.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import cv2
+# import cv2
 # from tensorflow.keras.models import load_model
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
@@ -32,11 +32,11 @@ canvas_result = st_canvas(
     drawing_mode="freedraw" if mode else "transform",
     key='canvas')
 
-if canvas_result.image_data is not None:
-    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-    rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-    st.write('Model Input')
-    st.image(rescaled)
+# if canvas_result.image_data is not None:
+#     img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+#     rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+#     st.write('Model Input')
+#     st.image(rescaled)
 
 # if st.button('Predict'):
 #     test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
